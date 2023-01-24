639d8a7f5a1ca841ff49ba56dc1bab057122af3a
MatiMende5 <106565470+MatiMende5@users.noreply.github.com>
2023-01-24 18:54:20 -0300

    Delete src directory

--- src/Utils/eliminarProducto.js
+++
@@ -1,9 +0,0 @@
-import {deleteDoc, doc} from 'firebase/firestore';
-import {db} from '../components/firebase/config';
-
-const eliminarProducto = async (id) => {
-	const productDoc = doc(db, 'products', id);
-	await deleteDoc(productDoc);
-};
-
-export default eliminarProducto;
